7556b1bfc5f2602b9d3bf4f1bd9636eb322efdaf
26ghimireyd <26ghimireyd@bwschools.net>
2025-04-22 12:12:48 -0400

    commit 3

--- sprites.py
+++ sprites.py
@@ -104,43 +104,46 @@ class Player(pg.sprite.Sprite):
 class Platform(pg.sprite.Sprite):
     def __init__(self, x, y, w, h):
         pg.sprite.Sprite.__init__(self)
-        self.plat_img = []
+        # self.plat_img = []
         # self.image = pg.Surface((w, h))
         # self.image.fill(GREEN)
-        self.load_img()
-        self.image = ran.choice(self.plat_img)
+
+        self.image = pg.image.load("Sprites/Platforms/platform.png")
+
+        # self.load_img()
+        # self.image = ran.choice(self.plat_img)
 
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-    def load_img(self):
-        for i in range(1,5):
-            filename = f"Sprites/Platforms/platform{i}.png"
-            img = pg.image.load(filename)
-            # img = pg.transform.scale(img, (50,70))
-            self.plat_img.append(img)
+    # def load_img(self):
+    #     for i in range(1,5):
+    #         filename = f"Sprites/Platforms/platform{i}.png"
+    #         img = pg.image.load(filename)
+    #         # img = pg.transform.scale(img, (50,70))
+    #         self.plat_img.append(img)
 
 
 
-class SpriteSheet():
-  def __init__(self, image):
-    self.sheet = image
-  def get_image(self, frame,framey, width, height, scale, color):
-    width_use = int(width*scale)
-    height_use = int(height*scale)
-    image = pg.Surface((width, height)).convert_alpha()
-    image.blit(self.sheet, (0, 0), ((frame * width), (framey*height), width, height))
-    image = pg.transform.scale(image, (width_use, height_use))
-    image.set_colorkey(color)
-
-sprite_sheet_image = pg.image.load('spritesheet.png')
-sprite_sheet = spritesheet.SpriteSheet(sprite_sheet_image)
-# size = 1
-for x in range(25):
-    for y in range(32):
-        #426X629
-        # if x == 6 and y==0:
-        plats.append(sprite_sheet.get_image(x, y, 17.04, 17.03,SCALE, BLACK))
-
-        image = sprite_sheet.get_image(x, y, 17.04, 17.03,SCALE, BLACK)
-        image.set_colorkey(BLACK)
+# class SpriteSheet():
+#   def __init__(self, image):
+#     self.sheet = image
+#   def get_image(self, frame,framey, width, height, scale, color):
+#     width_use = int(width*scale)
+#     height_use = int(height*scale)
+#     image = pg.Surface((width, height)).convert_alpha()
+#     image.blit(self.sheet, (0, 0), ((frame * width), (framey*height), width, height))
+#     image = pg.transform.scale(image, (width_use, height_use))
+#     image.set_colorkey(color)
+#
+# sprite_sheet_image = pg.image.load('spritesheet.png')
+# sprite_sheet = spritesheet.SpriteSheet(sprite_sheet_image)
+# # size = 1
+# for x in range(25):
+#     for y in range(32):
+#         #426X629
+#         # if x == 6 and y==0:
+#         plats.append(sprite_sheet.get_image(x, y, 17.04, 17.03,SCALE, BLACK))
+#
+#         image = sprite_sheet.get_image(x, y, 17.04, 17.03,SCALE, BLACK)
+#         image.set_colorkey(BLACK)
